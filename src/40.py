bf6e4f0faf4d7cf8dd572e3b36d3d64ece7bb575
matusiaklafera601 <matusiaklafera601@gmail.com>
2025-05-25 19:38:54 +0000

    Add 40.py

---
+++ src/40.py
@@ -0,0 +1,15 @@
+def find_max_in_tuple(tup):
+    """
+    Find the maximum value in a tuple.
+    
+    Parameters:
+    tup (tuple): A tuple of numbers.
+    
+    Returns:
+    int: The maximum number in the tuple.
+    """
+    return max(tup)
+
+# Example usage:
+my_list = [10, 23, -5, 78, 14]
+print(find_max_in_tuple(my_list))  # Output: 78
